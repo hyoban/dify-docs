de9fab246360507190a27c98f8a1ba347694a43e
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-28 01:35:16 +0800

    chore: root meta

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -1,5 +1,6 @@
 import { generateFiles } from 'fumadocs-openapi';
 import { createOpenAPI } from 'fumadocs-openapi/server';
+import fs from 'node:fs'
 
 const languages = ['en', 'zh', 'ja'];
 const apiName = ['chat', 'chatflow', 'completion', 'knowledge', 'workflow']
@@ -20,11 +21,17 @@ const openapiWithPath = languages.flatMap((lang) =>
 }));
 
 openapiWithPath.forEach(({ openapi, lang, apiName }) => {
-  void generateFiles({
+  generateFiles({
     input: openapi,
     output: `./content/docs/${lang}/api-reference/${apiName}`,
     // we recommend to enable it
     // make sure your endpoint description doesn't break MDX syntax.
     includeDescription: true,
-  });
+  }).then(() => {
+    // create meta.json file
+    const meta = {
+      'root': true,
+    };
+    fs.writeFileSync(`./content/docs/${lang}/api-reference/meta.json`, JSON.stringify(meta, null, 2));
+  })
 });
