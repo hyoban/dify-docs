fc2d71a8a06ba2c6d9cbaeb0ecda0c020bd721e2
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-28 01:25:40 +0800

    chore: index redirect

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,30 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en/use-dify/getting-started/introduction',
+        permanent: false,
+      },
+      {
+        source: '/en',
+        destination: '/en/use-dify/getting-started/introduction',
+        permanent: false,
+      },
+      {
+        source: '/zh',
+        destination: '/zh/use-dify/getting-started/introduction',
+        permanent: false,
+      },
+      {
+        source: '/ja',
+        destination: '/ja/use-dify/getting-started/introduction',
+        permanent: false,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
